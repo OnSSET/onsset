102087ba20d69a697e7a889e5f4dba7b6b3c9450
Alexandros Korkovelos <alekor@desa.kth.se>
2018-01-23 19:59:59 +0100

    Update conf.py

--- OnSSET_Manual/conf.py
+++ OnSSET_Manual/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'OnSSET'
 copyright = '2017, KTH-dESA'
-author = 'Konstantinos Filippos Pegios'
+author = 'KTH Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
