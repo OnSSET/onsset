f895d738c22b38fa15c2f06359084905058e1f2c
Oluchi Peace Monwe <oluchi@UG.KTH.SE>
2019-12-13 13:38:53 +0100

    added a test for land_cover

---
+++ test/test_land_cover.py
@@ -0,0 +1,40 @@
+import os
+
+from onsset import SettlementProcessor
+
+from pandas import DataFrame,Series
+from pandas.testing import assert_frame_equal, assert_series_equal
+from pytest import fixture
+
+
+class TestSettlementProcessor:
+
+    @fixture
+    def setup_settlementprocessor(self) -> SettlementProcessor:
+
+        csv_path = os.path.join('test', 'test_data', 'dj-test.csv')
+        settlementprocessor = SettlementProcessor(csv_path)
+
+        return settlementprocessor
+
+    @fixture
+    def setup_dataframe(self) -> DataFrame:
+        df = DataFrame({
+                        'LandCover':list(range(17))
+                                               
+                         })
+        return df
+   
+    def test_classify_land_cover(self,setup_settlementprocessor,setup_dataframe):
+        sp = setup_settlementprocessor
+        df = setup_dataframe
+
+        actual = sp.classify_land_cover(df['LandCover'])
+
+        print (actual)
+
+        expected =Series([1,3,4,3,4,3,2,5,2,5,5,1,3,3,5,3,5], name='LandCover')
+
+        print (expected)
+        assert_series_equal(actual, expected)
+       
