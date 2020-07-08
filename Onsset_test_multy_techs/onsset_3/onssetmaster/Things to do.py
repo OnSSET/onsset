0dcc1f5ac4097093937fcfd06cc01658ca8b5d34
ares <sergiob44_47@hotmail.com>
2020-07-08 15:03:06 -0400

    Delete tests

--- Onsset_test_multy_techs/onsset_3/onssetmaster/Things to do.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 29 19:49:30 2020
-
-@author: balderrama
-"""
-# ask about the hydropower mvline problem in the older model
-# check SET_HYDRO to big numbers if it is kW
